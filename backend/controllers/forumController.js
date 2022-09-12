084f10a413b053abdbf51d7c894d855f3bfcfc51
“Philippe <philippehcov@gmail.com>
2022-09-12 12:32:53 -0700

    Forum Controller/Routes Setup

--- backend/controllers/forumController.js
+++ backend/controllers/forumController.js
@@ -1,31 +1,38 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc Get Forums
 //@route GET /api/forums
 //@access Private
-const getForum = (req,res) =>{
+const getForum = asyncHandler(async (req,res) =>{
     res.status(200).json({ message: 'Get Forums'})
-}
+})
 
 //@desc set Forums
 //@route POST /api/forums
 //@access Private
-const setForum = (req, res) => {
+const setForum = asyncHandler(async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({message: 'Set Forum'})
-}
+})
 
 
 //@desc Update Forums
 //@route PUT /api/forums/:id
 //@access Private
-const updateForum = (req,res) =>{
+const updateForum = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `Update Forum ${req.params.id}`})
-}
+})
 
 //@desc Delete Forums
 //@route DELETE /api/forums/:id
 //@access Private
-const deleteForum = (req,res) =>{
+const deleteForum = asyncHandler(async (req,res) =>{
     res.status(200).json({message: `Delete Forum ${req.params.id}`})
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/forums', require('./routes/forumRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
