76f4acfe356ae6d63585282d4f61778957d4d161
“Philippe <philippehcov@gmail.com>
2022-09-13 17:59:34 -0700

    Forum Routes Protected

--- backend/routes/forumRoutes.js
+++ backend/routes/forumRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { getForum, setForum, updateForum, deleteForum } = require('../controllers/forumController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getForum).post(setForum)
-router.route('/:id').delete(deleteForum).put(updateForum)
+
+router.route('/').get(protect, getForum).post(protect, setForum)
+router.route('/:id').delete(protect, deleteForum).put(protect, updateForum)
 
 
 
