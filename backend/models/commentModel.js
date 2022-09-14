efb5bb7d41e9eec81281c4c5e8efd7811a8d90ed
“Philippe <philippehcov@gmail.com>
2022-09-13 23:39:23 -0700

    Front End Setup

---
+++ backend/models/commentModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const commentSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User', 
+    },
+    text:{
+        type: String,
+        required: [true, 'Please add text value']
+    }
+},{
+    timestamps: true,
+}
+)
+
+module.exports = mongoose.model('Forum', forumSchema)
