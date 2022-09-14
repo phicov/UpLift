a88bc59ca3714a43a19e3719e4de49ba1cf7f8cc
“Philippe <philippehcov@gmail.com>
2022-09-14 00:40:25 -0700

    Auth Slice/Service Connected

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
@@ -17,7 +18,8 @@ export const register = createAsyncThunk('auth/register', async(user, thunkAPI) 
     try {
         return await authService.register(user)
     } catch (error) {
-        
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
     }
 })
 
