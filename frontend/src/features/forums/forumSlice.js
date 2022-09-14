42c010be9b96ed51817900c765f2be344a0b4c28
“Philippe <philippehcov@gmail.com>
2022-09-14 02:11:55 -0700

    Forum Creation Work

--- frontend/src/features/forums/forumSlice.js
+++ frontend/src/features/forums/forumSlice.js
@@ -8,6 +8,18 @@ const initialState = {
     message: '',
 }
 
+// Create new forum
+
+export const createForum = createAsyncThunk('forums/create', async (goalData, thunkAPI) => {
+    try{
+        const token = thunkAPI.getState().auth.user.token
+        return await forumService.createForum(forumData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const forumSlice = createSlice ({
     name:
         'forum', 
