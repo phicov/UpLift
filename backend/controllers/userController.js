f4fd7d64f6cc63716a9ecaebea68e722549ee0e2
“Philippe <philippehcov@gmail.com>
2022-09-13 17:18:36 -0700

    User Creater

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,8 +26,23 @@ const registerUser = asyncHandler(async( req, res ) => {
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
 
+    // Create User
+    const user = await User.create({
+        name, 
+        email,
+        password: hashedPassword
+    })
 
-    res.json({message: 'Register User'})
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
 })
 
 //@desc Authenticate a user
