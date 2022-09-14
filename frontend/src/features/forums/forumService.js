e378f68bd1d039d91876396b288c45fdd6a17a06
“Philippe <philippehcov@gmail.com>
2022-09-14 03:10:10 -0700

    Forum Service Fix

--- frontend/src/features/forums/forumService.js
+++ frontend/src/features/forums/forumService.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 const API_URL = '/api/forums/'
 
-// create new forum
+// Create new goal
 const createForum = async (forumData, token) => {
     const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
     }
 
     const response = await axios.post(API_URL, forumData, config)
@@ -15,12 +15,12 @@ const createForum = async (forumData, token) => {
     return response.data
 }
 
-// Get user Forums
+// Get user goals
 const getForums = async (token) => {
     const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
     }
 
     const response = await axios.get(API_URL, config)
@@ -28,10 +28,23 @@ const getForums = async (token) => {
     return response.data
 }
 
+// Delete user Forum
+const deleteForum = async (forumId, token) => {
+    const config = {
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+    }
 
-const forumService = {
+    const response = await axios.delete(API_URL + forumId, config)
+
+    return response.data
+}
+
+const goalService = {
     createForum,
-    getForums
+    getForums,
+    deleteForum,
 }
 
 export default forumService
