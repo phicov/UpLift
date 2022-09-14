5c85ac63e3404fb82d2f05dec3dba18324ea2527
“Philippe <philippehcov@gmail.com>
2022-09-14 01:28:20 -0700

    Login Setup Stuff

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -23,6 +23,16 @@ export const register = createAsyncThunk('auth/register', async(user, thunkAPI) 
     }
 })
 
+// Login user
+export const login = createAsyncThunk('auth/login', async(user, thunkAPI) => {
+    try {
+        return await authService.login(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 
 export const authSlice = createSlice({
     name: 'auth',
@@ -51,7 +61,24 @@ export const authSlice = createSlice({
             state.message = action.payload
             state.user = null 
         })
-    }
+        .addCase(login.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(login.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload 
+        })
+        .addCase(login.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+            state.user = null 
+        })
+        .addCase(logout.fulfilled, (state) => {
+            state.user = null
+        })
+    },
 })
 
 export const logout = createAsyncThunk('/auth/logout',
